e2fd35f7e61e0a0b389ebfa5fa9524a67beccc7e
rajat sehdev <rajat.sehdev@tothenew.com>
2022-05-14 00:47:12 +0530

    Updates

---
+++ precache-manifest.425cec971a856807f58f61b2387e3321.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d66921f28aefad05b8aa783110938b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada2983ed395dd6bfd11",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "75b6bb38f655aee8682d",
+    "url": "/static/css/main.d6942d54.chunk.css"
+  },
+  {
+    "revision": "ada2983ed395dd6bfd11",
+    "url": "/static/js/2.d661ebd4.chunk.js"
+  },
+  {
+    "revision": "cc1d475654d16100cd3145963202e796",
+    "url": "/static/js/2.d661ebd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b6bb38f655aee8682d",
+    "url": "/static/js/main.96da0e02.chunk.js"
+  },
+  {
+    "revision": "e6f34118dcf825308980",
+    "url": "/static/js/runtime-main.908cba80.js"
+  }
+]);
